de4a506ca2a2f722d0661199729e8be97dc110d0
Kundan Tamsoy <105445488+Tomkndn@users.noreply.github.com>
2024-11-25 06:54:31 +0530

    [changes] - python config changed

--- downloader/app.py
+++ downloader/app.py
@@ -32,5 +32,5 @@ def greet():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     # Run the server
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False)
+    app.run(port=port)
