988f93f2ad0fee316103eb1d0c9a3f0602de5dee
TusharS004 <124758085+TusharS004@users.noreply.github.com>
2024-11-25 18:41:25 +0530

    [updated instagram service]

--- backend/src/services/instagram.service.js
+++ backend/src/services/instagram.service.js
@@ -1,5 +1,37 @@
+// const fetchInstagramDataWithMedia = async (url) => {
+//     console.log(`Hello inside fetchInstagramDataWithMedia ${url} Fetching Instagram data with media.`);
+//     const download_media = await fetch(process.env.FLASK_URL + "/api/url", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ url: url }),
+//     });
+//     console.log("after flask");
+    
+//     if(!download_media) {
+//         console.error("Error fetching Instagram data.");
+//         return;
+//     }
+
+//     console.log(download_media);
+//     const data = download_media;
+//     console.log(download_media.json());
+    
+
+//     // const data = download_media.json();
+//     if (!data.message) {
+//         console.error("Error fetching Instagram data. ", data);
+//         return;
+//     }
+
+//     return data;
+// };
+
+// export default fetchInstagramDataWithMedia;
+
 const fetchInstagramDataWithMedia = async (url) => {
-    console.log(`Hello inside fetchInstagramDataWithMedia ${url} Fetching Instagram data with media.`);
+    console.log(`\n\n\nHello inside fetchInstagramDataWithMedia \n\n${url}\n\n Fetching Instagram data with media.\n\n\n`);
     const download_media = await fetch(process.env.FLASK_URL + "/api/url", {
         method: "POST",
         headers: {
@@ -7,7 +39,7 @@ const fetchInstagramDataWithMedia = async (url) => {
         },
         body: JSON.stringify({ url: url }),
     });
-    console.log("after flask");
+    console.log("\n\nafter flask\n\n");
     
     if(!download_media) {
         console.error("Error fetching Instagram data.");
